c1fc848896a2b7e77eaf4d341b09bf12bc6db357
felipekgouvea <felipekgouvea@gmail.com>
2022-08-31 20:00:46 -0300

    Firt component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,15 @@
+import "./style.css";
+import icon from '../../assets/img/notification-icon.svg';
+
+const NotificationButton = () => {
+
+  return (
+    <div className="dsmeta-red-btn-container">
+    <div className="dsmeta-red-btn">
+      <img src={icon} alt="Notificar" />
+    </div>
+  </div>
+  )
+}
+
+export default NotificationButton;
